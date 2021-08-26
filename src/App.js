d7aecaa16108911c0ca35dd5a77c1688d5a094ca
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-08-27 01:30:49 +0530

    feat: routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Quiz from './pages/quiz';
+import Hypotenuse from './pages/hypotenuse';
+import Area from './pages/area';
+import Angles from './pages/angles';
 
 function App() {
   return (
     <Router>
-      <h1> Hello ji </h1>
+      <Switch>
+        <Route path="/" exact component={Quiz} />
+        <Route path="/hypotenuse" component={Hypotenuse} />
+        <Route path="/area" component={Area} />
+        <Route path="/angles" component={Angles} />
+      </Switch>
     </Router>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div>
+            <h1> Navbar </h1>
+        </div>
+    );
+}
+
+export default Navbar;
