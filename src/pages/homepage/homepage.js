4c394adb56741893ec1ca8a98d19b6eda6785fbe
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-09-11 01:01:31 +0530

    style: homepage css done

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Navbar from '../../components/navbar';
 import './homepage.css';
+import { Link } from 'react-router-dom';
 
 function Homepage() {
     return (
-        <div className="main">
-            <Navbar />
-            <h1> Homepage </h1>
+        <div className="main" >
+            <div id="about">
+                <h1> Mad angle Triangle </h1>
+                <p>
+                    Welcome to Mad angle triangles. Click on the different options on the right to learn
+                     about triangles and its various properties in a fun way. Explore and share this with your friends if you found it helpful.
+</p>
+            </div>
+            <div className="linkContainer">
+                <Link to="/quiz" className="link">  Quiz </Link>
+                <Link to="/area" className="link">  Area </Link>
+                <Link to="/hypotenuse" className="link">  Hypotenuse </Link>
+                <Link to="/angles" className="link">  Angles </Link>
 
-        </div>
+            </div>
+        </div >
 
     )
 }
