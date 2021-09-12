8cf30b2bc78e38b8472b09036d3b1dc660053854
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-09-13 02:00:42 +0530

    style: area css done

--- src/pages/area/area.js
+++ src/pages/area/area.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from '../../components/navbar';
+import './area.css';
 
 function Area(e) {
 
@@ -13,6 +14,10 @@ function Area(e) {
 
     function handleSubmit(e) {
         e.preventDefault();
+        if (side1 < 0 || side2 < 0 || side3 < 0 || side1 == undefined || side2 == undefined || side3 == undefined) {
+            alert('Please enter valid input values.');
+            return 0;
+        }
         var s = (side1 + side2 + side3) / 2;
         console.log(side1 + side2 + side3);
         setArea(Math.sqrt(s * (s - side1) * (s - side2) * (s - side3)));
@@ -20,6 +25,10 @@ function Area(e) {
     }
 
     function findArea(e) {
+        if(base < 0 || perpendicular < 0 || base == undefined || perpendicular == undefined){
+            alert('Please enter valid input values.');
+            return 0;
+        }
         e.preventDefault();
         setAr(0.5 * base * perpendicular);
     }
@@ -27,33 +36,40 @@ function Area(e) {
     return (
         <div>
             <Navbar />
-            <h1> Area </h1>
-            <h2> Using Heron's formula </h2>
-            <form>
-                <label> Enter side 1 (cm)</label>
-                <input required onChange={(e) => setSide1(parseInt(e.target.value))} type="number" /><br />
-                <label> Enter side 2 (cm)</label>
-                <input required onChange={(e) => setSide2(parseInt(e.target.value))} type="number" /><br />
-                <label> Enter side 3 (cm)</label>
-                <input required onChange={(e) => setSide3(parseInt(e.target.value))} type="number" /><br />
-                <button onClick={handleSubmit}> Calculate Area </button>
-                <div hidden={area == undefined ? true : false}>
-                    <h2> Area of triangle using Heron's formula = {area} square centimeter</h2>
+
+            <h1 id="areaHeading"> Area </h1>
+            <div style={{ 'display': 'flex' }}>
+                <div className="areaBox">
+                    <h2> Using Heron's formula </h2>
+                    <form>
+                        <label> Enter side 1 (cm)</label>
+                        <input className="areaIP" required onChange={(e) => setSide1(parseInt(e.target.value))} type="number" /><br />
+                        <label> Enter side 2 (cm)</label>
+                        <input className="areaIP" required onChange={(e) => setSide2(parseInt(e.target.value))} type="number" /><br />
+                        <label> Enter side 3 (cm)</label>
+                        <input className="areaIP" required onChange={(e) => setSide3(parseInt(e.target.value))} type="number" /><br />
+                        <center><button id="submit" onClick={handleSubmit}> Calculate Area </button></center>
+                        <div hidden={area == undefined ? true : false}>
+                            <p> Area of triangle using Heron's formula = {area} square centimeter</p>
+                        </div>
+                        {/* EDITS REQUIRED HERE --> when sides dont make a triangle or zero */}
+                    </form>
                 </div>
-                {/* EDITS REQUIRED HERE --> when sides dont make a triangle or zero */}
-            </form>
-            <h2> Using base and perpendicular(for right triangles) </h2 >
-            <form>
-                <label> Enter length of base (cm) </label>
-                <input required onChange={(e) => setBase(parseInt(e.target.value))} type="number" /><br />
-                <label> Enter length of perpendicular (cm)</label>
-                <input required onChange={(e) => setPerpendicular(parseInt(e.target.value))} type="number" /><br />
-                <button onClick={findArea}> Calculate area </button>
-                <div hidden={ar == undefined ? true : false}>
-                    <h2> Area of triangle = {ar} square centimeter </h2>
+                <div className="areaBox">
+                    <h2> Using base and perpendicular(for right triangles) </h2 >
+                    <form>
+                        <label> Enter length of base (cm) </label>
+                        <input className="areaIP" required onChange={(e) => setBase(parseInt(e.target.value))} type="number" /><br />
+                        <label> Enter length of perpendicular (cm)</label>
+                        <input className="areaIP" required onChange={(e) => setPerpendicular(parseInt(e.target.value))} type="number" /><br />
+                        <center><button id="submit" onClick={findArea}> Calculate area </button></center>
+                        <div hidden={ar == undefined ? true : false}>
+                            <p> Area of triangle = {ar} square centimeter </p>
+                        </div>
+                        {/* EDITS REQUIRED HERE --> check conditions for base and perpendicular */}
+                    </form>
                 </div>
-                {/* EDITS REQUIRED HERE --> check conditions for base and perpendicular */}
-            </form>
+            </div>
         </div >
     );
 }
